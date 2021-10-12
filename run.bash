b24cf91227a36b8a3d18c79b9047bda12696dad3
LTS <72404520+LTSCommerce@users.noreply.github.com>
2021-10-12 12:13:33 +0100

    Update run.bash
    switching to https clone, adding section titles

--- run.bash
+++ run.bash
@@ -8,6 +8,13 @@ IFS=$'\n\t'
 ## Assertions
 ## Functions
 
+title(){
+  printf "\n$1\n"
+}
+
+done(){
+  printf "\nDone...\n"
+}
 
 ## Process
 
@@ -18,17 +25,24 @@ Process Starting
 You will be asked for sudo password
 
 "
-
+title "Installing Dependencies with DNF"
 sudo dnf -y install \
   git \
   python3 \
   python3-pip
+done
   
+title "Installing Ansible with Pip"
 pip3 install ansible
+done
 
+title "Creating Projects directory"
 mkdir ~/Projects
 cd ~/Projects
+done
 
-git clone git@github.com:LongTermSupport/fedora-desktop.git
+title "Cloning Fedora Desktop Repo"
+git clone https://github.com/LongTermSupport/fedora-desktop.git
+done
 
 cd fedora-desktop
