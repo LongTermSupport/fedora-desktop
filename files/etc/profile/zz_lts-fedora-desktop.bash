80bf118443785d539671ce3fc11f04c5b1a739f1
Joseph Edmonds <joseph@ltscommerce.dev>
2023-06-13 17:16:21 +0100

    remove check to prevent multiple sourcing - doesnt work and stops it loading - also new alias for dmesg

--- files/etc/profile/zz_lts-fedora-desktop.bash
+++ files/etc/profile/zz_lts-fedora-desktop.bash
@@ -1,110 +1,107 @@
 ### --main
 
-# Only apply configs once even if sourced multiple times
-if [[ -z ${lts_bash_tweaks_active+x} ]]; then
-  export lts_bash_tweaks_active=1
 
-  # User specific aliases and functions
-  alias rm='rm -i'
-  alias cp='cp -i'
-  alias mv='mv -i'
-  alias ll='ls -alh --color'
+# User specific aliases and functions
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+alias ll='ls -alh --color'
 
-  # Make parent dirs if they are missing
-  alias mkdir='mkdir -pv'
+#real time stamps in dmesg
+alias dmesg='dmesg -T'
 
-  # Handle UTF-8 with less
-  export LESSCHARSET=utf-8
+# Make parent dirs if they are missing
+alias mkdir='mkdir -pv'
 
-  #History
-  shopt -s histappend
-  shopt -s cmdhist
-  HISTCONTROL=ignoredups
-  export HISTFILESIZE=20000
-  export HISTSIZE=10000
-  export HISTIGNORE="&:ls:[bf]g:exit"
+# Handle UTF-8 with less
+export LESSCHARSET=utf-8
 
-  # Composer global install
-  if [[ -d ~/.config/composer/vendor/bin/ ]];
-  then
-      PATH="$PATH:$HOME/.config/composer/vendor/bin/"
-  fi
+#History
+shopt -s histappend
+shopt -s cmdhist
+HISTCONTROL=ignoredups
+export HISTFILESIZE=20000
+export HISTSIZE=10000
+export HISTIGNORE="&:ls:[bf]g:exit"
 
-  # RVM bin folder
-  if [[ -d ~/.rvm/bin ]];
-  then
-      PATH=$PATH:$HOME/.rvm/bin
-  fi
+# Composer global install
+if [[ -d ~/.config/composer/vendor/bin/ ]];
+then
+    PATH="$PATH:$HOME/.config/composer/vendor/bin/"
+fi
 
-  # Settings for interactive shell only inside this block
-  if [[ $- == *i* ]]
-  then
+# RVM bin folder
+if [[ -d ~/.rvm/bin ]];
+then
+    PATH=$PATH:$HOME/.rvm/bin
+fi
 
-      #Prompt
-      function redPrompt(){
-          export PS1='\[\e[1m\]$PWD\[\e[0m\]'"\n\[\033[38;5;1m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]@\[$(tput sgr0)\]\[\033[38;5;9m\]\h\[$(tput sgr0)\] "
-      }
-      function bluePrompt(){
-          export PS1='\[\e[1m\]$PWD\[\e[0m\]'"\n\[\033[38;5;32m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]@\[$(tput sgr0)\]\[\033[38;5;32m\]\h\[$(tput sgr0)\] "
-      }
-      if [[ "$(whoami)" == "root" ]]
-      then
-          redPrompt
-      else
-          bluePrompt
-      fi
+# Settings for interactive shell only inside this block
+if [[ $- == *i* ]]
+then
 
-      #Prevent Ctrl+S Freezing things
-      stty -ixon
+    #Prompt
+    function redPrompt(){
+        export PS1='\[\e[1m\]$PWD\[\e[0m\]'"\n\[\033[38;5;1m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]@\[$(tput sgr0)\]\[\033[38;5;9m\]\h\[$(tput sgr0)\] "
+    }
+    function bluePrompt(){
+        export PS1='\[\e[1m\]$PWD\[\e[0m\]'"\n\[\033[38;5;32m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]@\[$(tput sgr0)\]\[\033[38;5;32m\]\h\[$(tput sgr0)\] "
+    }
+    if [[ "$(whoami)" == "root" ]]
+    then
+        redPrompt
+    else
+        bluePrompt
+    fi
 
-      # fix spelling errors for cd, only in interactive shell
-      shopt -s cdspell
+    #Prevent Ctrl+S Freezing things
+    stty -ixon
 
-      # More useful bash completelion setting
-      bind "set completion-ignore-case on" # note: bind used instead of sticking these in .inputrc
-      bind "set bell-style none" # no bell
-      bind "set show-all-if-ambiguous On" # show list automatically, without double tab
+    # fix spelling errors for cd, only in interactive shell
+    shopt -s cdspell
 
-      alias gti=git
+    # More useful bash completelion setting
+    bind "set completion-ignore-case on" # note: bind used instead of sticking these in .inputrc
+    bind "set bell-style none" # no bell
+    bind "set show-all-if-ambiguous On" # show list automatically, without double tab
 
-      complete -r cd  # completion on symlinks is unusual and a __complete__ pain in the arse. Let's remove it
+    alias gti=git
 
-      export EDITOR=vim
-      alias vi="vim"
-      
-      # Docker Node stuff
-      DOCKER_NODE_VER=${DOCKER_NODE_VER:-16}
-      docker-node-version() {
-        case "$1" in
-          -s|--set)
-            if [ "$2" ]; then
-                DOCKER_NODE_VER="$2"
-              echo 
-              echo "docker-node will now use node:$2 image!"
-            fi
-            return 0
-        esac
-        echo $DOCKER_NODE_VER
-      }
-      docker-node-image() {
-        echo -n "node:$(docker-node-version)"
-      }
-      docker-node-run() {
-      set -x
-        local dp
-        [ "$1" = "bash" ] && dp="-it"
-        dp="$dp --rm"
-        dp="$dp -v "$PWD":/usr/src/app"
-        dp="$dp -w /usr/src/app"
-        docker run $dp $(docker-node-image) "$@"
-      set +x
-      }
-      node() { docker-node-run "$@"; }
-      npm() { docker-node-run npm "$@"; }
-      npx() { docker-node-run npx "$@"; }
-      yarn() { docker-node-run yarn "$@"; }
+    complete -r cd  # completion on symlinks is unusual and a __complete__ pain in the arse. Let's remove it
 
+    export EDITOR=vim
+    alias vi="vim"
 
-      
-  fi
+    # Docker Node stuff
+    DOCKER_NODE_VER=${DOCKER_NODE_VER:-16}
+    docker-node-version() {
+      case "$1" in
+        -s|--set)
+          if [ "$2" ]; then
+              DOCKER_NODE_VER="$2"
+            echo
+            echo "docker-node will now use node:$2 image!"
+          fi
+          return 0
+      esac
+      echo $DOCKER_NODE_VER
+    }
+    docker-node-image() {
+      echo -n "node:$(docker-node-version)"
+    }
+    docker-node-run() {
+    set -x
+      local dp
+      [ "$1" = "bash" ] && dp="-it"
+      dp="$dp --rm"
+      dp="$dp -v "$PWD":/usr/src/app"
+      dp="$dp -w /usr/src/app"
+      docker run $dp $(docker-node-image) "$@"
+    set +x
+    }
+    node() { docker-node-run "$@"; }
+    npm() { docker-node-run npm "$@"; }
+    npx() { docker-node-run npx "$@"; }
+    yarn() { docker-node-run yarn "$@"; }
+
 fi
