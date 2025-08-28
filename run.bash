a2be36946a87c32819dbd6d567042373a8dd6412
LTS <72404520+LTSCommerce@users.noreply.github.com>
2025-08-28 17:39:57 +0100

    Update run.bash
    DNF5 changes

--- run.bash
+++ run.bash
@@ -103,7 +103,7 @@ fi
 
 title "Installing Github CLI"
 sudo dnf -y install 'dnf-command(config-manager)'
-sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+sudo dnf config-manager addrepo --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo
 sudo dnf -y install gh
 completed
 
